18081212c94d2de9cbaaad51d36f466d5142635f
jinwhan804 <jinwhan804@hanmail.net>
2024-01-04 18:24:31 +0900

    project add

--- src/contents/projects.jsx
+++ src/contents/projects.jsx
@@ -10,6 +10,7 @@ import Modal from '../components/modal'
 const Projects = () => {
     const [isShowingModal, toggleModal] = useModal()
     const [isShowingModal2, toggleModal2] = useModal()
+    const [isShowingModal3, toggleModal3] = useModal()
 
     return (
         <Wrapper>
@@ -40,7 +41,7 @@ const Projects = () => {
                     </div>
                     <div className="w-full">
                         <h2 className="flex flex-row items-center gap-[8px] text-[16px] text-slate-800 md:text-black md:text-[24px] font-semibold pb-[16px]">
-                            바운스 익스플로러 (협약 프로젝트){' '}
+                            Bounce Explorer (협약 프로젝트){' '}
                             <a href="https://www.bouncexplorer.site">
                                 <FaExternalLinkAlt />
                             </a>
@@ -73,7 +74,7 @@ const Projects = () => {
                     </div>
                 </li>
 
-                {/* 첫번째 프로젝트 */}
+                {/* 두번째 프로젝트 */}
                 <li className="flex flex-col gap-[32px] md:flex-row">
                     <div className="w-full md:w-[420px] ">
                         <img
@@ -97,7 +98,7 @@ const Projects = () => {
                     </div>
                     <div className="w-full">
                         <h2 className="flex flex-row items-center gap-[8px] text-[16px] text-slate-800 md:text-black md:text-[24px] font-semibold pb-[16px]">
-                            steam100 (react 프로젝트){' '}
+                            Steam100 (react 프로젝트){' '}
                             <a href="https://steamclone.com">
                                 <FaExternalLinkAlt />
                             </a>
@@ -122,8 +123,62 @@ const Projects = () => {
                         <h3 className="py-[16px] text-[16px] font-bold">주요 업무</h3>
                         <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
                             <li>유저 관리와 게시물 관리를 위한 Database를 구축했습니다.</li>
-                            <li>유저 관리 기능 (로그인, 로그아웃, 가입 승인 등)과 게시물 관리 기능 (게시글 추가, 수정, 삭제 등)의 기능을 구현했습니다.</li>
+                            <li>유저 관리 기능 (로그인, 로그아웃, 가입 승인 등)과 게시물 관리 기능 (CRUD)의 기능을 구현했습니다.</li>
                             <li>Front-end 서버에서 게시물 기능 일부를 (좋아요, 대댓글 등) 브라우저에 정상적으로 표기되도록 수정했습니다.</li>
+                            <li>AWS EC2를 통해 Front-end 서버와 Back-end 서버를 배포하였습니다.</li>
+                        </ul>
+                    </div>
+                </li>
+
+                {/* 세번째 프로젝트 */}
+                <li className="flex flex-col gap-[32px] md:flex-row">
+                    <div className="w-full md:w-[420px] ">
+                        <img
+                            src="https://moccasin-odd-penguin-381.mypinata.cloud/ipfs/QmUaQtGJrEK3SjnJyNArhWaVWe9ZUh1YCA8BbkdQH6GSHd"
+                            className="w-full"
+                            alt="어떤어떤 프로젝트"
+                        />
+                        <ul className="flex text-[12px] text-slate-600 py-[8px] gap-[8px] flex-row md:flex-col items-center md:items-start">
+                            <li className="text-[16px]">팀플 프로젝트 (3명)</li>
+                            <li className="text-stone-500">2023. 06. 05 ~ 2023. 06. 26</li>
+                            <li>front-end, back-end</li>
+                            <li>
+                                {/* <button
+                                    className="px-[16px] py-[8px] bg-gray-800 rounded-[8px] text-white hover:bg-gray-950"
+                                    onClick={toggleModal3}
+                                >
+                                    상세보기
+                                </button> */}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="w-full">
+                        <h2 className="flex flex-row items-center gap-[8px] text-[16px] text-slate-800 md:text-black md:text-[24px] font-semibold pb-[16px]">
+                            Simple Market (nodeJS 프로젝트){' '}
+                            <a href="">
+                                <FaExternalLinkAlt />
+                            </a>
+                        </h2>
+
+                        <p className="leading-5 text-slate-950 text-[12px] md:text-[16px] md:leading-7">
+                            중고 거래 사이트인 당근마켓의 기능 구현을 목표로 한 유저간 거래 사이트입니다.
+                        </p>
+
+                        <h3 className="py-[16px] text-[16px] font-bold">사용 스택</h3>
+                        <div className="flex flex-row text-[32px] gap-[8px] py-[8px] px-[32px]">
+                            <DiJavascript />
+                            <DiNodejs />
+                            <DiGithubBadge />
+                            <SiExpress />
+                            <SiMysql />
+                            <SiAmazonec2 />
+                        </div>
+
+                        <h3 className="py-[16px] text-[16px] font-bold">주요 업무</h3>
+                        <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
+                            <li>유저 관리와 게시물 관리를 위한 Database를 구축했습니다.</li>
+                            <li>게시물 관리 기능(CRUD)을 fallstack으로 작업을 진행, 구현했습니다.</li>                            
+                            <li>게시물 통계 페이지를 구현했습니다.</li>                            
                             <li>AWS EC2를 통해 Front-end 서버와 Back-end 서버를 배포하였습니다.</li>
                         </ul>
                     </div>
@@ -136,6 +191,10 @@ const Projects = () => {
 
             <Modal show={isShowingModal2} onCloseButtonClick={toggleModal2}>
                 test2222
+            </Modal>
+
+            <Modal show={isShowingModal3} onCloseButtonClick={toggleModal3}>
+                test3
             </Modal>
         </Wrapper>
     )
